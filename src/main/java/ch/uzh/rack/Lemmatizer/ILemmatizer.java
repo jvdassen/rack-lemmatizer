2238e1802e98c711ae41562c3b6909d20e2e0a92
Jan von der Assen <jan.vda@gmx.ch>
2019-03-25 16:31:09 +0100

    Update pom.xml and rename package

--- src/main/java/ch/uzh/rack/Lemmatizer/ILemmatizer.java
+++
@@ -1,7 +0,0 @@
-package ch.uzh.rack.Lemmatizer;
-
-import java.util.List;
-
-public interface ILemmatizer {
-	List<String> lemmatize(String s);
-}
--- src/main/java/ch/uzh/rack/Lemmatizer/IStopWordProvider.java
+++
@@ -1,8 +0,0 @@
-package ch.uzh.rack.Lemmatizer;
-
-import java.util.List;
-
-public interface IStopWordProvider {
-	public boolean isStopWord(String word);
-	public List<String> getStopWords();
-}
